b336b07f0d7271ff65a762cd1c14f9e999169bde
Ankit Savaliya <ankit.savaliya@amfredericks.com>
2020-06-02 14:28:59 -0400

    bug fix

--- public/js/custom.js
+++ public/js/custom.js
@@ -433,12 +433,13 @@ $('#insured_isRiskAddressSame').change(function() {
         44 comma 
         47 backslash
         38 &
+        46 dot
         39 single quote '
         dash - 45
         48-57 - (0-9)Numbers
       */
       //var allowedCharacter = [65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,32,127,44,47,38,39,45,48,49,50,51,52,53,54,55,56,57];
-      var allowedCharacter = [32,127,44,47,38,39,45];
+      var allowedCharacter = [32,127,44,47,38,39,45,46];
       if ( (!(keyCode >= 48 && keyCode <= 57)) && (!(keyCode >= 65 && keyCode <= 90)) && (!(keyCode >= 97 && keyCode <= 122)) && jQuery.inArray(keyCode,allowedCharacter) == -1) { 
         return false;
       }
